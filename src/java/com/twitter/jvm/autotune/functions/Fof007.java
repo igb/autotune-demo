e5f533a5ab755c545be506912991bd0c36121948
ibrown <ibrown@twitter.com>
2018-10-20 21:39:17 -0700

    adding generated functions

---
+++ src/java/com/twitter/jvm/autotune/functions/Fof007.java
@@ -0,0 +1,151 @@
+import java.awt.geom.*;
+
+public class Fof007 {
+	public static Point2D.Double[] points = {
+		new Point2D.Double(-247.334375, 154.896875),
+		new Point2D.Double(-244.993750, 154.771875),
+		new Point2D.Double(-241.910937, 154.234375),
+		new Point2D.Double(-238.432813, 153.664062),
+		new Point2D.Double(-235.706250, 152.951562),
+		new Point2D.Double(-232.814063, 151.862500),
+		new Point2D.Double(-230.818750, 151.065625),
+		new Point2D.Double(-228.271875, 150.237500),
+		new Point2D.Double(-224.609375, 148.920312),
+		new Point2D.Double(-222.079688, 147.942187),
+		new Point2D.Double(-219.373437, 146.860938),
+		new Point2D.Double(-217.184375, 145.973437),
+		new Point2D.Double(-216.648438, 145.551562),
+		new Point2D.Double(-214.300000, 144.585938),
+		new Point2D.Double(-210.756250, 143.107812),
+		new Point2D.Double(-204.592187, 140.703125),
+		new Point2D.Double(-194.860938, 136.010938),
+		new Point2D.Double(-180.390625, 127.573437),
+		new Point2D.Double(-169.575000, 120.737500),
+		new Point2D.Double(-158.918750, 111.614063),
+		new Point2D.Double(-154.562500, 106.950000),
+		new Point2D.Double(-153.950000, 105.642188),
+		new Point2D.Double(-148.585938, 104.807812),
+		new Point2D.Double(-141.982812, 102.914062),
+		new Point2D.Double(-129.739062, 98.573437),
+		new Point2D.Double(-113.403125, 91.215625),
+		new Point2D.Double(-100.790625, 83.437500),
+		new Point2D.Double(-92.200000, 76.607812),
+		new Point2D.Double(-86.610938, 71.676563),
+		new Point2D.Double(-84.975000, 70.154687),
+		new Point2D.Double(-84.643750, 69.823437),
+		new Point2D.Double(-82.346875, 69.006250),
+		new Point2D.Double(-69.331250, 61.139063),
+		new Point2D.Double(-56.396875, 52.296875),
+		new Point2D.Double(-47.157812, 45.750000),
+		new Point2D.Double(-39.645313, 39.253125),
+		new Point2D.Double(-36.453125, 35.721875),
+		new Point2D.Double(-34.890625, 33.354687),
+		new Point2D.Double(-33.746875, 29.385938),
+		new Point2D.Double(-29.331250, 21.137500),
+		new Point2D.Double(-24.982813, 14.214063),
+		new Point2D.Double(-20.573438, 8.450000),
+		new Point2D.Double(-14.785937, 1.907812),
+		new Point2D.Double(-9.331250, -3.210938),
+		new Point2D.Double(-5.742188, -6.007812),
+		new Point2D.Double(-3.065625, -7.854687),
+		new Point2D.Double(-2.403125, -8.185937),
+		new Point2D.Double(0.239063, -13.501563),
+		new Point2D.Double(5.234375, -19.218750),
+		new Point2D.Double(13.521875, -26.520312),
+		new Point2D.Double(24.125000, -35.685938),
+		new Point2D.Double(33.456250, -43.125000),
+		new Point2D.Double(40.676563, -49.520313),
+		new Point2D.Double(44.637500, -53.812500),
+		new Point2D.Double(45.960938, -55.409375),
+		new Point2D.Double(46.073437, -55.965625),
+		new Point2D.Double(46.353125, -56.432812),
+		new Point2D.Double(55.934375, -68.004688),
+		new Point2D.Double(74.125000, -88.518750),
+		new Point2D.Double(88.428125, -103.729687),
+		new Point2D.Double(102.098438, -116.687500),
+		new Point2D.Double(109.943750, -122.209375),
+		new Point2D.Double(114.403125, -125.489063),
+		new Point2D.Double(114.898438, -126.065625),
+		new Point2D.Double(115.367188, -126.065625),
+		new Point2D.Double(119.864063, -126.920312),
+		new Point2D.Double(136.400000, -133.643750),
+		new Point2D.Double(153.014062, -141.117188),
+		new Point2D.Double(165.496875, -147.360938),
+		new Point2D.Double(172.303125, -150.931250),
+		new Point2D.Double(173.975000, -151.806250),
+		new Point2D.Double(174.139062, -151.806250),
+		new Point2D.Double(179.126563, -151.806250),
+		new Point2D.Double(189.225000, -152.843750),
+		new Point2D.Double(200.542188, -155.571875),
+		new Point2D.Double(210.826562, -157.770312),
+		new Point2D.Double(218.940625, -159.618750),
+		new Point2D.Double(224.154687, -160.584375),
+		new Point2D.Double(225.464063, -160.709375),
+		new Point2D.Double(225.635938, -160.507812),
+		new Point2D.Double(229.001563, -159.320312),
+		new Point2D.Double(233.329688, -156.901563),
+		new Point2D.Double(236.492188, -154.878125),
+		new Point2D.Double(238.090625, -153.521875),
+		new Point2D.Double(239.140625, -152.825000),
+		new Point2D.Double(240.564063, -151.990625),
+		new Point2D.Double(242.429688, -151.110938),
+		new Point2D.Double(244.001563, -150.618750),
+		new Point2D.Double(245.431250, -150.148438),
+		new Point2D.Double(246.487500, -149.792188),
+		new Point2D.Double(247.850000, -149.103125),
+		new Point2D.Double(248.640625, -148.420312),
+		new Point2D.Double(249.425000, -147.401563),
+		new Point2D.Double(250.209375, -146.498437),
+		new Point2D.Double(250.764062, -145.825000),
+		new Point2D.Double(251.773438, -145.035937),
+		new Point2D.Double(252.556250, -144.587500),
+		new Point2D.Double(253.320312, -144.045312),
+		new Point2D.Double(254.081250, -143.609375),
+		new Point2D.Double(254.717187, -143.182813),
+		new Point2D.Double(255.020312, -143.079688),
+		new Point2D.Double(255.117188, -142.975000),
+	};
+
+	public static Double interpolate(Double x, Point2D.Double a, Point2D.Double b) {
+		return a.y + ((b.y - a.y) *   ((x - a.x) / (b.x - a.x)));
+	}
+
+	public static Point2D.Double findA(Double x, Point2D.Double[] points) {
+		Point2D.Double a = null;
+		for (int i = 0; i < points.length; i++) {
+			if (points[i].x > x) {
+				return a;
+
+			} else {
+				a = points[i];
+			}
+
+		}
+		return a;
+	}
+
+	public static Point2D.Double findB(Double x, Point2D.Double[] points) {
+		for (int i = 0; i < points.length; i++) {
+			if (points[i].x > x) {
+				return points[i];
+			}
+		
+		}
+		return null;
+	}
+
+	public Double getYforX(Double x) {
+		Point2D.Double a = findA(x, points);
+		Point2D.Double b = findB(x, points);
+		return interpolate(x, a, b);
+	}
+
+	public static void main(String[] args) {
+		Double x = Double.parseDouble(args[0]);
+		Point2D.Double a = findA(x, points);
+		Point2D.Double b = findB(x, points);
+		Double y = interpolate(x, a, b);
+		System.out.println(y);
+	}
+
+}
---
+++ src/java/com/twitter/jvm/autotune/functions/Fof008.java
@@ -0,0 +1,185 @@
+import java.awt.geom.*;
+
+public class Fof008 {
+	public static Point2D.Double[] points = {
+		new Point2D.Double(-489.450000, -303.475000),
+		new Point2D.Double(-481.265625, -300.403125),
+		new Point2D.Double(-473.825000, -295.468750),
+		new Point2D.Double(-470.200000, -292.268750),
+		new Point2D.Double(-466.006250, -289.012500),
+		new Point2D.Double(-461.615625, -285.343750),
+		new Point2D.Double(-455.209375, -279.118750),
+		new Point2D.Double(-447.440625, -270.040625),
+		new Point2D.Double(-435.493750, -258.062500),
+		new Point2D.Double(-426.675000, -248.896875),
+		new Point2D.Double(-416.953125, -238.193750),
+		new Point2D.Double(-401.131250, -222.640625),
+		new Point2D.Double(-378.306250, -196.512500),
+		new Point2D.Double(-370.171875, -183.612500),
+		new Point2D.Double(-364.034375, -172.790625),
+		new Point2D.Double(-358.300000, -163.356250),
+		new Point2D.Double(-351.465625, -153.771875),
+		new Point2D.Double(-344.471875, -144.412500),
+		new Point2D.Double(-337.375000, -132.015625),
+		new Point2D.Double(-329.931250, -121.268750),
+		new Point2D.Double(-322.465625, -112.031250),
+		new Point2D.Double(-316.465625, -102.950000),
+		new Point2D.Double(-316.053125, -102.531250),
+		new Point2D.Double(-310.012500, -88.137500),
+		new Point2D.Double(-302.328125, -74.875000),
+		new Point2D.Double(-297.584375, -65.681250),
+		new Point2D.Double(-295.150000, -59.990625),
+		new Point2D.Double(-291.587500, -53.337500),
+		new Point2D.Double(-288.196875, -46.743750),
+		new Point2D.Double(-284.378125, -39.775000),
+		new Point2D.Double(-281.581250, -35.906250),
+		new Point2D.Double(-276.359375, -21.665625),
+		new Point2D.Double(-270.956250, -6.196875),
+		new Point2D.Double(-263.575000, 12.721875),
+		new Point2D.Double(-255.034375, 30.900000),
+		new Point2D.Double(-244.115625, 48.525000),
+		new Point2D.Double(-242.503125, 51.471875),
+		new Point2D.Double(-238.028125, 65.890625),
+		new Point2D.Double(-226.837500, 87.096875),
+		new Point2D.Double(-211.340625, 118.050000),
+		new Point2D.Double(-195.218750, 143.534375),
+		new Point2D.Double(-183.706250, 159.075000),
+		new Point2D.Double(-178.800000, 173.934375),
+		new Point2D.Double(-166.415625, 196.887500),
+		new Point2D.Double(-153.259375, 218.684375),
+		new Point2D.Double(-141.421875, 233.609375),
+		new Point2D.Double(-138.390625, 236.446875),
+		new Point2D.Double(-130.096875, 248.525000),
+		new Point2D.Double(-124.596875, 256.525000),
+		new Point2D.Double(-115.612500, 266.359375),
+		new Point2D.Double(-103.834375, 277.959375),
+		new Point2D.Double(-101.065625, 280.415625),
+		new Point2D.Double(-100.509375, 280.603125),
+		new Point2D.Double(-95.506250, 284.459375),
+		new Point2D.Double(-91.818750, 287.468750),
+		new Point2D.Double(-86.968750, 291.153125),
+		new Point2D.Double(-80.500000, 295.759375),
+		new Point2D.Double(-74.671875, 299.900000),
+		new Point2D.Double(-72.228125, 301.468750),
+		new Point2D.Double(-71.815625, 301.468750),
+		new Point2D.Double(-71.446875, 301.468750),
+		new Point2D.Double(-66.368750, 303.153125),
+		new Point2D.Double(-60.690625, 305.618750),
+		new Point2D.Double(-55.025000, 306.856250),
+		new Point2D.Double(-50.587500, 307.781250),
+		new Point2D.Double(-47.937500, 308.871875),
+		new Point2D.Double(-46.040625, 309.290625),
+		new Point2D.Double(-44.346875, 309.290625),
+		new Point2D.Double(-42.696875, 309.290625),
+		new Point2D.Double(-40.553125, 309.290625),
+		new Point2D.Double(-38.159375, 309.290625),
+		new Point2D.Double(-36.515625, 309.290625),
+		new Point2D.Double(-33.746875, 309.962500),
+		new Point2D.Double(-31.143750, 311.028125),
+		new Point2D.Double(-29.509375, 311.446875),
+		new Point2D.Double(-26.118750, 312.340625),
+		new Point2D.Double(-21.890625, 312.825000),
+		new Point2D.Double(-18.656250, 312.328125),
+		new Point2D.Double(-18.287500, 312.328125),
+		new Point2D.Double(-12.734375, 312.328125),
+		new Point2D.Double(-10.106250, 312.328125),
+		new Point2D.Double(-8.215625, 312.328125),
+		new Point2D.Double(-6.065625, 312.328125),
+		new Point2D.Double(-4.165625, 312.328125),
+		new Point2D.Double(-1.015625, 312.328125),
+		new Point2D.Double(1.428125, 312.328125),
+		new Point2D.Double(3.784375, 312.328125),
+		new Point2D.Double(5.212500, 312.328125),
+		new Point2D.Double(5.993750, 312.328125),
+		new Point2D.Double(7.178125, 312.328125),
+		new Point2D.Double(7.971875, 312.328125),
+		new Point2D.Double(10.115625, 311.484375),
+		new Point2D.Double(13.806250, 309.662500),
+		new Point2D.Double(17.534375, 307.159375),
+		new Point2D.Double(18.425000, 305.587500),
+		new Point2D.Double(18.609375, 305.587500),
+		new Point2D.Double(24.028125, 303.712500),
+		new Point2D.Double(29.331250, 298.659375),
+		new Point2D.Double(37.675000, 292.390625),
+		new Point2D.Double(52.978125, 284.878125),
+		new Point2D.Double(62.687500, 276.806250),
+		new Point2D.Double(63.306250, 276.187500),
+		new Point2D.Double(69.831250, 260.425000),
+		new Point2D.Double(80.381250, 245.021875),
+		new Point2D.Double(88.462500, 232.468750),
+		new Point2D.Double(90.231250, 227.262500),
+		new Point2D.Double(108.518750, 198.290625),
+		new Point2D.Double(138.615625, 156.318750),
+		new Point2D.Double(152.425000, 136.268750),
+		new Point2D.Double(153.803125, 133.300000),
+		new Point2D.Double(165.365625, 102.925000),
+		new Point2D.Double(184.328125, 67.825000),
+		new Point2D.Double(198.103125, 45.334375),
+		new Point2D.Double(201.415625, 37.496875),
+		new Point2D.Double(209.137500, 14.115625),
+		new Point2D.Double(238.975000, -25.503125),
+		new Point2D.Double(260.334375, -53.990625),
+		new Point2D.Double(265.840625, -63.065625),
+		new Point2D.Double(288.887500, -104.818750),
+		new Point2D.Double(329.059375, -153.328125),
+		new Point2D.Double(354.181250, -185.856250),
+		new Point2D.Double(359.271875, -194.262500),
+		new Point2D.Double(367.925000, -206.846875),
+		new Point2D.Double(390.350000, -236.818750),
+		new Point2D.Double(428.350000, -279.246875),
+		new Point2D.Double(438.790625, -289.643750),
+		new Point2D.Double(438.975000, -289.643750),
+		new Point2D.Double(454.921875, -295.071875),
+		new Point2D.Double(471.684375, -302.918750),
+		new Point2D.Double(483.118750, -309.065625),
+		new Point2D.Double(497.421875, -315.734375),
+		new Point2D.Double(499.271875, -316.350000),
+		new Point2D.Double(509.987500, -320.000000),
+		new Point2D.Double(517.690625, -323.468750),
+		new Point2D.Double(523.390625, -327.171875),
+		new Point2D.Double(526.262500, -330.009375),
+	};
+
+	public static Double interpolate(Double x, Point2D.Double a, Point2D.Double b) {
+		return a.y + ((b.y - a.y) *   ((x - a.x) / (b.x - a.x)));
+	}
+
+	public static Point2D.Double findA(Double x, Point2D.Double[] points) {
+		Point2D.Double a = null;
+		for (int i = 0; i < points.length; i++) {
+			if (points[i].x > x) {
+				return a;
+
+			} else {
+				a = points[i];
+			}
+
+		}
+		return a;
+	}
+
+	public static Point2D.Double findB(Double x, Point2D.Double[] points) {
+		for (int i = 0; i < points.length; i++) {
+			if (points[i].x > x) {
+				return points[i];
+			}
+		
+		}
+		return null;
+	}
+
+	public Double getYforX(Double x) {
+		Point2D.Double a = findA(x, points);
+		Point2D.Double b = findB(x, points);
+		return interpolate(x, a, b);
+	}
+
+	public static void main(String[] args) {
+		Double x = Double.parseDouble(args[0]);
+		Point2D.Double a = findA(x, points);
+		Point2D.Double b = findB(x, points);
+		Double y = interpolate(x, a, b);
+		System.out.println(y);
+	}
+
+}
