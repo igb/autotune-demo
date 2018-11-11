273243f75b659ad2907154c98d5e07ff82bcd115
ibrown <ibrown@twitter.com>
2018-11-11 14:35:48 -0800

    re-doing functions (and experiments) to provide only positive objective function score

---
+++ src/java/com/twitter/jvm/autotune/YComparator.java
@@ -0,0 +1,17 @@
+package com.twitter.jvm.autotune;
+
+import java.awt.geom.Point2D;
+import java.util.Comparator;
+
+public class YComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+       if  (((Point2D)o1).getY() < ((Point2D)o2).getY()) {
+           return -1;
+       } else if (((Point2D)o1).getY() > ((Point2D)o2).getY()) {
+           return 1;
+       } else {
+           return 0;
+       }
+    }
+}
